60dd81b86f7fd7d58b054cb1fc3b77f8e29bc312
Alexander Komegunov <a_komegunov@twistellar.com>
2022-12-21 23:47:01 +0300

    added prettier config

--- force-app/main/default/lwc/migrateObjectTool/migrateObjectTool.js
+++ force-app/main/default/lwc/migrateObjectTool/migrateObjectTool.js
@@ -1,18 +1,21 @@
 import { LightningElement } from 'lwc'
 import getObjectNames from '@salesforce/apex/MigrateCustomObjectController.getObjectNames'
 
-const CUSTOM_OBJECT = 'custom_object';
-const BIG_OBJECT = 'big_object';
+const CUSTOM_OBJECT = 'custom_object'
+const BIG_OBJECT = 'big_object'
 
 export default class MigrateObjectTool extends LightningElement {
-
   sObjectNameOptions = []
   bigObjectNameOptions = []
   loading = false
 
   async connectedCallback() {
-    await this._getSObjectNames()
-    await this._getBigObjectNames()
+    let promises = []
+
+    promises.push(this._getSObjectNames())
+    promises.push(this._getBigObjectNames())
+
+    Promise.all(promises).finally(() => (this.loading = false))
   }
 
   async _getSObjectNames() {
@@ -20,11 +23,9 @@ export default class MigrateObjectTool extends LightningElement {
       this.loading = true
       const sObjectNames = await getObjectNames({ objectType: CUSTOM_OBJECT })
       this.sObjectNameOptions = JSON.parse(sObjectNames)
-      console.log(s(this.sObjectNameOptions));
+      console.log(s(this.sObjectNameOptions))
     } catch (error) {
       console.error('error in fetching sObject names', error)
-    } finally {
-      this.loading = false
     }
   }
 
@@ -33,24 +34,21 @@ export default class MigrateObjectTool extends LightningElement {
       this.loading = true
       const bigObjectNames = await getObjectNames({ objectType: BIG_OBJECT })
       this.bigObjectNameOptions = JSON.parse(bigObjectNames)
-      console.log(s(this.bigObjectNameOptions));
+      console.log(s(this.bigObjectNameOptions))
     } catch (error) {
       console.error('error in fetching bigObject names', error)
-    } finally {
-      this.loading = false
     }
   }
 
   handleSObjectChange(event) {
     const newValue = event.detail.value
-    console.log('co', newValue);
+    console.log('co', newValue)
   }
 
   handleBigObjectChange(event) {
     const newValue = event.detail.value
-    console.log('bo', newValue);
+    console.log('bo', newValue)
   }
-
 }
 
 function s(o) {
