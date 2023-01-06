244c8c1bec56d9f911a8ace3a70d1277c63e2f72
Alexander Komegunov <a_komegunov@twistellar.com>
2023-01-06 19:30:33 +0300

    hack

--- force-app/main/default/lwc/utils/labelUtils.js
+++ force-app/main/default/lwc/utils/labelUtils.js
@@ -2,7 +2,7 @@ export function formatLabel(str) {
   const args = Array.prototype.slice.call(arguments, 1)
   let replacements = args
   if (Array.isArray(args[0])) {
-    [replacements] = args
+    ;[replacements] = args
   }
 
   return str.replace(/{(\d+)}/g, (match, i) => {
